c74f0ac48b9fb1c1f0f29a539e4365db9e2ed4f3
HyFiveDev <kauan.souza@edu.fecap.br>
2026-09-03 17:15:35 -0300

    commit

---
+++ Rio de Prata - Fecart/Assets/scripts/TrocadeCena.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrocadeCena : MonoBehaviour
+{
+    
+    public void IrParaFase()
+    {
+        SceneManager.LoadScene("Fase 01");
+    }
+}
